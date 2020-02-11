a88605789480974a689c0719803b751f755ab43e
Thibaud <thibaud.ferreri@orange.fr>
2020-02-11 16:57:28 +0100

    AvancementBoutonsCréationGroupe

--- app/src/main/java/com/example/projetandroid/CreatingGroup.java
+++ app/src/main/java/com/example/projetandroid/CreatingGroup.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.LinearLayout;
 import android.widget.EditText;
 import android.widget.Button;
@@ -26,11 +27,27 @@ public class CreatingGroup extends AppCompatActivity {
         }
     }
 
-    public void onClickAddButton(View v){
-        LinearLayout addLayout = findViewById(R.id.addingLayout);
-        LinearLayout  hLayoutx = new LinearLayout(this);
+    public OnClickListener onClickAddButton(View v){
+        LinearLayout addingLayout = findViewById(R.id.addingLayout);
+        EditText prevPerson = findViewById(R.id.personne);
+        Button prevButton = findViewById(R.id.addButton);
+
+        LinearLayout  hLayout = new LinearLayout(this);
+        hLayout.setOrientation(LinearLayout.HORIZONTAL);
+
         EditText newPerson = new EditText(this);
-        
+        newPerson.setText(getResources().getString(R.string.guest_name_prompt));
+
+        Button newButton = new Button(this);
+        newButton.setText(prevButton.getText());
+        newButton.setOnClickListener(onClickAddButton(v));
+                /*new OnClickLister(){
+            public void onClickAddButtonAssign(View v){
+                onClickAddButton(v);
+            }
+        });*/
+
+                return this.onClickAddButton(v);
     }
 
     public void onClickValidateButton(View v){
