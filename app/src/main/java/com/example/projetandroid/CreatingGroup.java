2baedb457be366d12fc3a1aaa819803a7bea2421
Unknown <luluma582@gmail.com>
2020-02-12 14:10:57 +0100

    changement creating

--- app/src/main/java/com/example/projetandroid/CreatingGroup.java
+++ app/src/main/java/com/example/projetandroid/CreatingGroup.java
@@ -7,11 +7,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.EditText;
 import android.widget.Button;
 
+import java.util.ArrayList;
+
 public class CreatingGroup extends AppCompatActivity {
+
+    private Group groupe;
+    private ArrayList<String> colaborators = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +29,14 @@ public class CreatingGroup extends AppCompatActivity {
         }
     }
 
-    public OnClickListener onClickAddButton(View v){
+    public void onClickAddButton(View v){
+
         LinearLayout addingLayout = findViewById(R.id.addingLayout);
-        EditText prevPerson = findViewById(R.id.personne);
-        Button prevButton = findViewById(R.id.addButton);
+        ViewGroup lasthLayout = (ViewGroup) addingLayout.getChildAt(addingLayout.getChildCount()-1);
+        EditText prevPerson = (EditText) lasthLayout.getChildAt(0);
+        Button prevButton = (Button) lasthLayout.getChildAt(1);
+
+        colaborators.add(prevPerson.getText().toString());
 
         LinearLayout  hLayout = new LinearLayout(this);
         hLayout.setOrientation(LinearLayout.HORIZONTAL);
@@ -36,14 +46,20 @@ public class CreatingGroup extends AppCompatActivity {
 
         Button newButton = new Button(this);
         newButton.setText(prevButton.getText());
-        newButton.setOnClickListener(onClickAddButton(v));
-                /*new OnClickLister(){
-            public void onClickAddButtonAssign(View v){
+        newButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 onClickAddButton(v);
             }
-        });*/
+        });
 
-                return this.onClickAddButton(v);
+        hLayout.addView(newPerson);
+        hLayout.addView(newButton);
+
+        addingLayout.addView(hLayout);
+
+        prevPerson.setEnabled(false);
+        prevButton.setText("Retirer l\'invité");
     }
 
     public void onClickValidateButton(View v){
